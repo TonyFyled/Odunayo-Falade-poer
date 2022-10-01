e5d882c3753761fd07b36abe338cf1123a6a511b
Falade adedamola <faladedamola05@gmail.com>
2022-10-01 15:10:29 +0100

    updated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const submit = document.getElementById('submit');
 const form = document.querySelector('.form-group')
 submit.addEventListener('click', check);
 function check(e){
-  if(UIname.value ==='' && UIemail.value==='' && UItext.value === ''){
+  if(UIname.value ==='' || UIemail.value==='' || UItext.value === ''){
    document.querySelector('.warning').style.display = 'block';
     e.preventDefault();
   }
